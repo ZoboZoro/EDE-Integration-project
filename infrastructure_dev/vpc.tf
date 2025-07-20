019df961cb282be502d469c7594ef67d23ea0763
Taofeecoh <adesanutaofeecoh@gmail.com>
2025-07-20 22:12:47 +0100

    vpc

---
+++ infrastructure_dev/vpc.tf
@@ -0,0 +1,68 @@
+
+# Create VPC
+resource "aws_vpc" "multisource_vpc" {
+  cidr_block       = "98.16.0.0/16"
+  instance_tenancy = "default"
+  enable_dns_hostnames = true
+  owner_id = var.account_id
+
+  tags = local.common_tags
+}
+
+
+# Create internet gateway
+resource "aws_internet_gateway" "multisource_gw" {
+  vpc_id = aws_vpc.multisource_vpc.id
+
+  tags = local.common_tags
+}
+
+resource "aws_internet_gateway_attachment" "multisource_gw_attachment" {
+  internet_gateway_id = aws_internet_gateway.multisource_gw.id
+  vpc_id              = aws_vpc.multisource_vpc.id
+}
+
+
+# Create Subnets
+resource "aws_subnet" "public_subnet1" {
+  vpc_id     = aws_vpc.multisource_vpc.id
+  cidr_block = "98.16.0.0/24"
+  map_public_ip_on_launch = true
+
+  tags = local.common_tags
+
+}
+
+resource "aws_subnet" "private_subnet1" {
+  vpc_id     = aws_vpc.multisource_vpc.id
+  cidr_block = "98.16.1.0/24"
+
+  tags = local.common_tags
+}
+
+
+# Route table and routes
+resource "aws_route_table" "multisource_rtb" {
+  vpc_id = aws_vpc.multisource_vpc.id
+
+  tags = local.common_tags
+}
+
+resource "aws_route" "multisource_route_toInternetgw" {
+  route_table_id            = aws_route_table.multisource_rtb.id
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.multisource_gw.id
+}
+
+resource "aws_route" "multisource_route_toNatgw" {
+  route_table_id            = aws_route_table.multisource_rtb.id
+  destination_cidr_block    = "98.16.1.0/24"
+  gateway_id = aws_internet_gateway.multisource_gw.id
+}
+
+
+# Associate route table
+resource "aws_route_table_association" "multisource_rtb" {
+  subnet_id      = aws_subnet.public_subnet1.id
+  route_table_id = aws_route_table.multisource_rtb.id
+}
