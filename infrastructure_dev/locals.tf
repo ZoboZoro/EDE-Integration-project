0cd752267a369d5a981cfb0140932f2395c3d44b
Taofeecoh <adesanutaofeecoh@gmail.com>
2025-07-24 16:02:24 +0100

    locals

--- infrastructure_dev/locals.tf
+++ infrastructure_dev/locals.tf
@@ -1,15 +1,15 @@
 locals {
-    Project_Name = "ede_multisource_project"
-    Managed_by   = "terrfaorm"
-    Environment  = "production"
+  Project_Name = "ede_multisource_project"
+  Managed_by   = "terrfaorm"
+  Environment  = "production"
 }
 
 locals {
   common_tags = {
     Project_Name = "ede_multisource_project"
     Managed_by   = "terrfaorm"
-    Owner = "Taofeecoh"
-    Github = "github.com/zobozoro"
+    Owner        = "Taofeecoh"
+    Github       = "github.com/zobozoro"
     Environment  = "production"
   }
 }
