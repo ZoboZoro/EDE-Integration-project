d73cd40a548dc6a3da581cb15407cae214741187
Taofeecoh <adesanutaofeecoh@gmail.com>
2025-07-30 18:42:12 +0100

    storage

---
+++ infrastructure_dev/storage.tf
@@ -0,0 +1,19 @@
+resource "aws_s3_bucket" "main" {
+  bucket = "general-dumpss"
+  force_destroy = true
+
+  tags = merge({Name = "general-dumpss"}, local.common_tags)
+    
+}
+
+resource "aws_s3_bucket_acl" "main" {
+  bucket = aws_s3_bucket.main.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_versioning" "main_versioning" {
+  bucket = aws_s3_bucket.main.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
