e2184aca132b0f9c838f86d6c30e9dba6e0e7452
Taofeecoh <adesanutaofeecoh@gmail.com>
2025-08-04 13:22:22 +0100

    vpc

--- infrastructure_dev/vpc.tf
+++ infrastructure_dev/vpc.tf
@@ -5,67 +5,44 @@ resource "aws_vpc" "multisource_vpc" {
   instance_tenancy     = "default"
   enable_dns_hostnames = true
 
-  tags = local.common_tags
+  tags = merge({Name = "multi_vpc"}, local.common_tags)
 }
 
 
-# Create internet gateway
+# Internet gateway
 resource "aws_internet_gateway" "multisource_gw" {
   vpc_id = aws_vpc.multisource_vpc.id
 
   tags = local.common_tags
 }
 
-# resource "aws_internet_gateway_attachment" "multisource_gw_attachment" {
-#   internet_gateway_id = aws_internet_gateway.multisource_gw.id
-#   vpc_id              = aws_vpc.multisource_vpc.id
-# }
 
-
-# Create Subnets
+# Subnet
 resource "aws_subnet" "public_subnet1" {
   vpc_id                  = aws_vpc.multisource_vpc.id
   cidr_block              = "98.16.0.0/24"
-  map_public_ip_on_launch = true
 
-  tags = local.common_tags
+  tags = merge({Name = "public_subnet1"}, local.common_tags)
 
 }
 
-resource "aws_subnet" "private_subnet1" {
-  vpc_id     = aws_vpc.multisource_vpc.id
-  cidr_block = "98.16.1.0/24"
-
-  tags = local.common_tags
-}
-
-
-# Route table and routes
+# Route table and route
 resource "aws_route_table" "multisource_rtb" {
   vpc_id = aws_vpc.multisource_vpc.id
 
-  tags = local.common_tags
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.multisource_gw.id
+  }
+
+  tags = merge({Name = "public_rtb"}, local.common_tags)
 }
-
-resource "aws_route" "multisource_route_toInternetgw" {
-  route_table_id         = aws_route_table.multisource_rtb.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.multisource_gw.id
-}
-
-# resource "aws_route" "multisource_route_toNatgw" {
-#   route_table_id         = aws_route_table.multisource_rtb.id
-#   destination_cidr_block = "98.16.1.0/24"
-#   gateway_id             = aws_internet_gateway.multisource_gw.id
-# }
-
 
 # Associate route table
 resource "aws_route_table_association" "multisource_rtb" {
   subnet_id      = aws_subnet.public_subnet1.id
   route_table_id = aws_route_table.multisource_rtb.id
 }
-
 
 # Subnet group
 # resource "aws_db_subnet_group" "subnet_g1" {
@@ -74,3 +51,12 @@ resource "aws_route_table_association" "multisource_rtb" {
 
 #   tags =merge({Name = "db_subnetgroup1"}, local.common_tags)
 # }
+
+resource "aws_redshift_subnet_group" "subnet_group" {
+  name        = "redshift-cluster-subnet-group"
+  description = "My Redshift cluster subnet group"
+  subnet_ids  = [aws_subnet.public_subnet1.id]
+
+  tags =merge({Name = "redshift-cluster-subnet-group"}, local.common_tags)
+}
+
