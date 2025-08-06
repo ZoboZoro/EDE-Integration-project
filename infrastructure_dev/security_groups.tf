fd80658c16e935e96b7a723015a0b5d8d5c6797c
Taofeecoh <adesanutaofeecoh@gmail.com>
2025-08-06 12:19:14 +0100

    sg

--- infrastructure_dev/security_groups.tf
+++ infrastructure_dev/security_groups.tf
@@ -8,7 +8,7 @@ resource "aws_security_group" "sg1" {
 
 }
 
-resource "aws_vpc_security_group_ingress_rule" "ingress1" {
+resource "aws_vpc_security_group_ingress_rule" "ingress1_https" {
   security_group_id = aws_security_group.sg1.id
   cidr_ipv4         = "0.0.0.0/0"
   from_port         = 443
@@ -16,7 +16,7 @@ resource "aws_vpc_security_group_ingress_rule" "ingress1" {
   to_port           = 443
 }
 
-resource "aws_vpc_security_group_ingress_rule" "ingress2" {
+resource "aws_vpc_security_group_ingress_rule" "ingress2_ssh" {
   security_group_id = aws_security_group.sg1.id
   cidr_ipv4         = aws_vpc.multisource_vpc.cidr_block
   from_port         = 22
@@ -24,11 +24,27 @@ resource "aws_vpc_security_group_ingress_rule" "ingress2" {
   to_port           = 22
 }
 
-resource "aws_vpc_security_group_ingress_rule" "ingress3" {
+resource "aws_vpc_security_group_ingress_rule" "ingress3_postgres" {
   security_group_id = aws_security_group.sg1.id
   cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1"
+  from_port         = 5432
+  ip_protocol       = "tcp"
+  to_port           = 5432
 }
+
+resource "aws_vpc_security_group_ingress_rule" "ingress4_redshift" {
+  security_group_id = aws_security_group.sg1.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 5439
+  ip_protocol       = "tcp"
+  to_port           = 5439
+}
+
+# resource "aws_vpc_security_group_ingress_rule" "ingress3" {
+#   security_group_id = aws_security_group.sg1.id
+#   cidr_ipv4         = "0.0.0.0/0"
+#   ip_protocol       = "-1"
+# }
 
 resource "aws_vpc_security_group_egress_rule" "egress1" {
   security_group_id = aws_security_group.sg1.id
