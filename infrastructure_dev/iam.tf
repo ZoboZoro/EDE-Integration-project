b7c87e8e42cf925fac49c3ad3c5c160f59736e43
Taofeecoh <adesanutaofeecoh@gmail.com>
2025-08-11 02:41:35 +0100

    iam

--- infrastructure_dev/iam.tf
+++ infrastructure_dev/iam.tf
@@ -21,6 +21,24 @@ resource "aws_iam_role" "s3_role" {
 resource "aws_iam_role" "schedule_role" {
   name = "schedule_role"
 
+
+  #  assume_role_policy = <<EOF
+  # {
+  #   "Version": "2012-10-17",
+  #   "Statement": [
+  #     {
+  #       "Action": "sts:AssumeRole",
+  #       "Principal": {
+  #         "Service": "ec2.amazonaws.com"
+  #       },
+  #       "Effect": "Allow",
+  #       "Sid": ""
+  #     }
+  #   ]
+  # }
+  # EOF
+
+
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -29,7 +47,7 @@ resource "aws_iam_role" "schedule_role" {
         Effect = "Allow"
         Sid    = ""
         Principal = {
-          Service = "scheduler.redshift.amazonaws.com"
+          Service = ["scheduler.redshift.amazonaws.com", "redshift.amazonaws.com"]
         }
       },
     ]
@@ -73,7 +91,7 @@ resource "aws_iam_policy" "redshift_s3_access_policy" {
 
 resource "aws_iam_policy" "schedule_policy" {
   name        = "redshift-schedule-policy"
-  description = "Policy to allow Redshift scheduler to pause and resume cluster"
+  description = "Policy to allow Redshift scheduler to pause, resume and resize cluster"
 
   policy = jsonencode({
     Version = "2012-10-17",
@@ -82,10 +100,10 @@ resource "aws_iam_policy" "schedule_policy" {
         Effect = "Allow",
         Action = [
           "redshift:PauseCluster",
-          "redshift:ResumeCluster"
+          "redshift:ResumeCluster",
+          "redshift:ResizeCluster"
         ],
-        Resource = "arn:aws:redshift:eu-central-1:517819573047:cluster/*"
-        #"arn:aws:redshift:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:cluster/*"
+        Resource = "arn:aws:redshift:eu-central-1:517819573047:cluster:tf-multisource-warehouse"
       }
     ]
   })
