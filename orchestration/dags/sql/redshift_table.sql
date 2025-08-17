042df28be2b4039acd4e5c1c94ff699a85ded637
Taofeecoh <adesanutaofeecoh@gmail.com>
2025-08-17 22:57:36 +0100

    redshift table

---
+++ orchestration/dags/sql/redshift_table.sql
@@ -0,0 +1,13 @@
+-- create table
+CREATE TABLE IF NOT EXISTS sub1_healthinfo(
+            names VARCHAR,
+            sex VARCHAR,
+            birthdate DATE,
+            blood_group VARCHAR,
+            ssn VARCHAR,
+            mail VARCHAR,
+            jobs VARCHAR,
+            address VARCHAR,
+            residence VARCHAR,
+            diagnosis VARCHAR
+        );
