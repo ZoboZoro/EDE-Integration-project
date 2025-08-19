955e0baecd4d882f8f7ea8ac7e6e077d3bbe5bc7
Taofeecoh <adesanutaofeecoh@gmail.com>
2025-08-19 16:49:40 +0100

    task

--- orchestration/dags/dag_tasks/task1.py
+++ orchestration/dags/dag_tasks/task1.py
@@ -1,22 +1,31 @@
 import time
 from datetime import datetime, timedelta
+from typing import Any
 
 import gspread
 from airflow.models import Variable
 from airflow.models.dag import DAG
 from airflow.operators.empty import EmptyOperator
-from airflow.operators.python import PythonOperator
+from airflow.operators.python import PythonOperator, PythonVirtualenvOperator
 from airflow.providers.amazon.aws.transfers.s3_to_redshift import \
     S3ToRedshiftOperator
 from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
 from dag_tasks import airflow_variables
-from utils.module import faker_to_s3, googlesheet_to_s3
+# from sqlalchemy import create_engine, String, DATE
+from utils.module import faker_to_s3, googlesheet_to_s3, googlesheet_to_db, googlesheet_db_withPGhook
 
 airflow_variables = airflow_variables
 
+# Constants and Vraiables
 BUCKET = "general-dumpss"
-REDSHIFT_TABLE = "healthinfo"
-IAM_ROLE_ARN = 'arn:aws:iam::517819573047:role/redshift_s3_role'
+REDSHIFT_TABLE = "reg1_healthinfo"
+POSTGRES_TABLE = "reg16_healthinfo"
+IAM_ROLE_ARN = "arn:aws:iam::517819573047:role/redshift_s3_role"
+USER = Variable.get('DB_USER')
+PG_SECRET = Variable.get('PG_SECRET')
+DB_HOST= Variable.get('DB_HOST')
+DB_NAME = Variable.get('DB_NAME')
 
 key = "healthinfo-records/healthinformatics{}.parquet".format(
     time.strftime("%Y-%m-%d_%H:%M:%S"))
@@ -30,16 +39,17 @@ filepath2 = "{}/healthinfo-gsheet/healthinformatics{}.parquet".format(
 client = gspread.service_account_from_dict(
         Variable.get("CREDENTIALS_AIRFLOW_GSERVICE", deserialize_json=True)
     )
+
 spreadsheet_source = "ptt_records"
 
+postgres_conn_id="postgres_default"
 
-copy_command = f"""
-COPY public.{REDSHIFT_TABLE}
-FROM '{filepath}'
-IAM_ROLE '{IAM_ROLE_ARN}'
-FORMAT AS PARQUET;
-"""
-
+to_rdsPG_args = {
+      "client": client,
+      "spreadsheet_name": spreadsheet_source,
+      "conn_id": postgres_conn_id,
+      "table": POSTGRES_TABLE,
+}
 
 default_args = {
     'owner': 'Taofeecoh',
@@ -69,6 +79,7 @@ with DAG(
                    },
     )
 
+
     drive_to_s3 = PythonOperator(
         task_id='drive_to_s3',
         provide_context=True,
@@ -81,12 +92,25 @@ with DAG(
     )
 
 
-create_table = SQLExecuteQueryOperator(
+create_redshift_table = SQLExecuteQueryOperator(
         task_id='create_redshift_table',
         conn_id='redshift_default',
-        sql='/sql_table.sql'
+        sql="sql/redshift_table.sql"
     )
 
+create_postgres_table = SQLExecuteQueryOperator(
+        task_id='create_postgres_table',
+        conn_id='postgres_default',
+        sql="sql/postgres.sql"
+    )
+
+
+to_postgres = PythonOperator(
+        task_id='load_to_postgres',
+        provide_context=True,
+        python_callable=googlesheet_db_withPGhook,
+        op_kwargs=to_rdsPG_args
+    )
 
 copy_to_redshift = S3ToRedshiftOperator(
         task_id='copy_data_to_redshift',
@@ -116,10 +140,18 @@ copy_to_redshift = S3ToRedshiftOperator(
 
 end = EmptyOperator(task_id='end')
 
-(
-    start
-    >> [to_s3, drive_to_s3]
-    >> create_table
-    >> copy_to_redshift
-    >> end
-)
+# (
+#     start 
+#     >>  to_s3 >> drive_to_s3
+#     << [create_postgres_table, create_redshift_table] 
+#     >> to_postgres << create_postgres_table >>
+#     copy_to_redshift << create_redshift_table
+#     << end
+# )
+
+start >> [to_postgres, copy_to_redshift]
+# start >> drive_to_s3 >> create_postgres_table >> to_postgres
+start >> [drive_to_s3, to_s3] >> create_postgres_table >> to_postgres
+#to_postgres << create_postgres_table << start
+copy_to_redshift << create_redshift_table << start
+
